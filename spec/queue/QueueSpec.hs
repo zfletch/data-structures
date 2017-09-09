f597c9b97f7f03c9b04c6f28316cb7a90b474867
zfletch <zfletch2@gmail.com>
2017-09-08 22:32:15 -0400

    refactor haskell queue

--- spec/queue/QueueSpec.hs
+++ spec/queue/QueueSpec.hs
@@ -26,8 +26,8 @@ spec = do
 
   describe "tail" $ do
     it "gets the tail" $ do
-      let queue = Queue.snoc 2 $ Queue.snoc 1 Queue.mkQueue
-      Queue.head (Queue.tail queue) == Just 2
+      let queue = Queue.snoc 3 $ Queue.tail $ Queue.snoc 2 $ Queue.snoc 1 Queue.mkQueue
+      Queue.head (Queue.tail queue) == Just 3
 
     it "returns empty for an empty queue" $ do
       let queue = Queue.mkQueue :: Queue.Queue Int
